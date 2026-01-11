34821d1be4c4f6c60dde35c5a057ae75cae0acdc
akshay1ks <akshay1119@outlook.com>
2026-01-12 02:33:07 +0530

    added button styling and press effects.

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,8 +1,41 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 function PrimaryButton({ children }) {
-    return <View>
-        <Text>{children}</Text>
-    </View>;
+    const pressHandler = () => {
+        console.log('Pressed!');
+    };
+
+    return (
+        <View style={styles.buttonOuterContainer}>
+            <Pressable 
+            onPress={pressHandler} 
+            android_ripple={{ color: '#d9006cff' }} 
+            style={({ pressed }) => pressed ? [styles.buttonPressed, styles.buttonInnerContainer] : styles.buttonInnerContainer}>
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>);
 }
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden',
+    },
+    buttonPressed: {
+        opacity: 0.75,
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#ba0861ff',
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        elevation: 2,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
 export default PrimaryButton;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,56 @@
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
-    return <View>
-        <TextInput />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+    return <View style={styles.inputContainer}>
+        <TextInput
+            style={styles.numberInput}
+            maxLength={2}
+            keyboardType='number-pad'
+            autoCapitalize='none'
+            autoCorrect={false} />
+        <View style={styles.buttonsContainer}>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton>Reset</PrimaryButton>
+            </View>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton>Confirm</PrimaryButton>
+            </View>
+        </View>
     </View>;
 }
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        elevation: 8,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 10,
+        shadowOpacity: 0.5,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1,
+    },
+});
 export default StartGameScreen;
